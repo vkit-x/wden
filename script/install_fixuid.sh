25d0c400fab2bb83ce68eea79740898664a2b73f
Hunt Zhan <huntzhan.dev@gmail.com>
2020-12-31 19:01:17 +0800

    decoration

--- script/install_fixuid.sh
+++ script/install_fixuid.sh
@@ -2,22 +2,18 @@
 set -e
 
 #https://github.com/boxboat/fixuid
-export USER=den
-export GROUP=den
-
 addgroup \
     --gid 1000 \
-    $GROUP
+    $FIXUID_GROUP
 
 adduser \
     --uid 1000 \
-    --ingroup $GROUP \
-    --home /home/"$USER" \
+    --ingroup $FIXUID_GROUP \
+    --home /home/"$FIXUID_USER" \
     --shell /usr/bin/zsh \
     --disabled-password \
     --gecos "" \
-    $USER
-
+    $FIXUID_USER
 
 export URL='https://github.com/boxboat/fixuid/releases/download/v0.5/fixuid-0.5-linux-amd64.tar.gz'
 curl -SsL $URL | tar -C /usr/local/bin -xzf -
@@ -25,4 +21,4 @@ chown root:root /usr/local/bin/fixuid
 chmod 4755 /usr/local/bin/fixuid
 
 mkdir -p /etc/fixuid
-printf "user: $USER\ngroup: $GROUP\n" > /etc/fixuid/config.yml
+printf "user: $FIXUID_USER\ngroup: $FIXUID_GROUP\n" > /etc/fixuid/config.yml
