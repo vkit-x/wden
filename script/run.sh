11f7c1ac1d85eede5e2fd2c55ab42378f3dad9df
Hunt Zhan <huntzhan.dev@gmail.com>
2020-12-31 19:49:37 +0800

    docker push

--- script/run.sh
+++ script/run.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Don't waste time to query nvidia repo.
-"$DEN_SCRIPT_FOLDER"/apt_disable_nvidia_repo.sh
+"$WDEN_SCRIPT_FOLDER"/apt_disable_nvidia_repo.sh
 
 apt-get update
 
-"$DEN_SCRIPT_FOLDER"/apt_install_pkgs_for_dev.sh
-"$DEN_SCRIPT_FOLDER"/install_python.sh
-"$DEN_SCRIPT_FOLDER"/configure_oh_my_zsh.sh
-"$DEN_SCRIPT_FOLDER"/configure_sudo.sh
-"$DEN_SCRIPT_FOLDER"/install_fixuid.sh
+"$WDEN_SCRIPT_FOLDER"/apt_install_pkgs_for_dev.sh
+"$WDEN_SCRIPT_FOLDER"/install_python.sh
+"$WDEN_SCRIPT_FOLDER"/configure_oh_my_zsh.sh
+"$WDEN_SCRIPT_FOLDER"/configure_sudo.sh
+"$WDEN_SCRIPT_FOLDER"/install_fixuid.sh
