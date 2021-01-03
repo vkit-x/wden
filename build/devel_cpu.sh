ec61430f58d081a822fe24668456f615aea0f78f
Hunt Zhan <huntzhan.dev@gmail.com>
2021-01-03 13:51:29 +0800

    rename

--- build/devel_cpu.sh
+++ build/devel_cpu.sh
@@ -3,8 +3,8 @@ set -e
 
 apt-get update
 
-"$WDEN_SCRIPT_FOLDER"/apt_install_pkgs_for_dev.sh
-"$WDEN_SCRIPT_FOLDER"/install_python.sh
-"$WDEN_SCRIPT_FOLDER"/configure_oh_my_zsh.sh
-"$WDEN_SCRIPT_FOLDER"/configure_sudo.sh
-"$WDEN_SCRIPT_FOLDER"/install_fixuid.sh
+"$WDEN_BUILD_FOLDER"/apt_install_pkgs_for_dev.sh
+"$WDEN_BUILD_FOLDER"/install_python.sh
+"$WDEN_BUILD_FOLDER"/configure_oh_my_zsh.sh
+"$WDEN_BUILD_FOLDER"/configure_sudo.sh
+"$WDEN_BUILD_FOLDER"/install_fixuid.sh
--- build/devel_cuda.sh
+++ build/devel_cuda.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Don't waste time to query nvidia repo.
-"$WDEN_SCRIPT_FOLDER"/apt_disable_nvidia_repo.sh
+"$WDEN_BUILD_FOLDER"/apt_disable_nvidia_repo.sh
 
 apt-get update
 
-"$WDEN_SCRIPT_FOLDER"/apt_install_pkgs_for_dev.sh
-"$WDEN_SCRIPT_FOLDER"/install_python.sh
-"$WDEN_SCRIPT_FOLDER"/configure_oh_my_zsh.sh
-"$WDEN_SCRIPT_FOLDER"/configure_sudo.sh
-"$WDEN_SCRIPT_FOLDER"/install_fixuid.sh
+"$WDEN_BUILD_FOLDER"/apt_install_pkgs_for_dev.sh
+"$WDEN_BUILD_FOLDER"/install_python.sh
+"$WDEN_BUILD_FOLDER"/configure_oh_my_zsh.sh
+"$WDEN_BUILD_FOLDER"/configure_sudo.sh
+"$WDEN_BUILD_FOLDER"/install_fixuid.sh
