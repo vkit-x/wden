ade7aae1c1e888d5ac287e15f53a7f27046b6086
Hunt Zhan <huntzhan.dev@gmail.com>
2021-01-04 21:40:33 +0800

    bugfix

--- build/configure_oh_my_bash.sh
+++ build/configure_oh_my_bash.sh
@@ -2,14 +2,16 @@
 set -e
 
 # Install oh-my-bash.
+export OSH='/.oh-my-bash'
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+mv /root/.bashrc /.bashrc
 
 # Grant all user the access.
-chmod -R 755 /root/.oh-my-bash
-chmod 755 /root/.bashrc
+chmod -R 755 /.oh-my-bash
+chmod 755 /.bashrc
 
 # Add to global startup script.
-printf '\n%s\n' 'source /root/.bashrc' >> /etc/bashrc
+printf '\n%s\n' 'source /.bashrc' >> /etc/profile
 
 # Initialize direnv.
-printf '\n%s\n' 'eval "$(direnv hook zsh)"' >> /root/.bashrc
+printf '\n%s\n' 'eval "$(direnv hook zsh)"' >> /.bashrc
--- build/devel_cpu.sh
+++ build/devel_cpu.sh
@@ -4,4 +4,4 @@ set -e
 "$WDEN_BUILD_FOLDER"/devel_shared.sh
 
 # printf '\n%s\n' 'source $WDEN_RUN_FOLDER/devel_cpu.sh' >> /opt/zsh/.zshrc
-printf '\n%s\n' 'source $WDEN_RUN_FOLDER/devel_cpu.sh' >> /root/.bashrc
+printf '\n%s\n' 'source $WDEN_RUN_FOLDER/devel_cpu.sh' >> /.bashrc
