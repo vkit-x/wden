8727ab26e4e6bc16c3bb0adcf0175dd380f23fc4
Hunt Zhan <huntzhan.dev@gmail.com>
2021-04-19 17:37:11 +0800

    Patch device permissions in privileged mode.

--- run/devel_shared.sh
+++ run/devel_shared.sh
@@ -8,19 +8,21 @@ source /.bashrc
 # Load direnv.
 eval "$(direnv hook bash)"
 
-# Setup actions.
+# APT mirror.
 if [ -n "$APT_SET_MIRROR_TENCENT" ] ; then
     sudo "$WDEN_RUN_FOLDER"/apt_set_mirror_tencent.sh
 elif [ -n "$APT_SET_MIRROR_ALIYUN" ] ; then
     sudo "$WDEN_RUN_FOLDER"/apt_set_mirror_aliyun.sh
 fi
 
+# PIP index.
 if [ -n "$PIP_SET_INDEX_TENCENT" ] ; then
     export PIP_INDEX_URL='https://mirrors.cloud.tencent.com/pypi/simple/'
 elif [ -n "$PIP_SET_INDEX_ALIYUN" ] ; then
     export PIP_INDEX_URL='https://mirrors.aliyun.com/pypi/simple/'
 fi
 
+# Home folder.
 if [ -n "$CD_DEFAULT_FOLDER" ] ; then
     if [ -d "$CD_DEFAULT_FOLDER" ] ; then
         alias cd="HOME='${CD_DEFAULT_FOLDER}' cd"
@@ -30,14 +32,33 @@ if [ -n "$CD_DEFAULT_FOLDER" ] ; then
     fi
 fi
 
-if [ -n "$SSH_AUTH_SOCK" ] ; then
-    if [ "$(stat -c '%a' "$SSH_AUTH_SOCK")" != '777' ] ; then
-        echo "WARNING: SSH_AUTH_SOCK=$SSH_AUTH_SOCK doesn't have permission 777. Auto fixing."
-        sudo chmod 777 "$SSH_AUTH_SOCK"
+function patch_file_permission {
+    FILE=$1
+    OCTAL_PERMISSIONS=$2
+    CUR_OCTAL_PERMISSIONS=$(stat -c '%a' "$FILE")
+    if [ "$CUR_OCTAL_PERMISSIONS" != "$OCTAL_PERMISSIONS" ] ; then
+        echo "WARNING: Patching the permissions of FILE=${FILE}" \
+             "${CUR_OCTAL_PERMISSIONS} => ${OCTAL_PERMISSIONS}"
+        sudo chmod "$OCTAL_PERMISSIONS" "$FILE"
     fi
+}
+
+# Patch device permissions in privileged mode.
+patch_file_permission /dev/console '620'
+patch_file_permission /dev/full '666'
+patch_file_permission /dev/null '666'
+patch_file_permission /dev/random '666'
+patch_file_permission /dev/tty '666'
+patch_file_permission /dev/urandom '666'
+patch_file_permission /dev/zero '666'
+
+# SSH agent.
+if [ -n "$SSH_AUTH_SOCK" ] ; then
+    patch_file_permission "$SSH_AUTH_SOCK" '777'
     export SSH_OPTIONS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 fi
 
+# SSH proxy.
 if [ -n "$SSH_SOCKS5_PROXY" ] ; then
     export SSH_OPTIONS="${SSH_OPTIONS} -o ProxyCommand='ncat --proxy-type socks5 --proxy ${SSH_SOCKS5_PROXY} %h %p'"
 fi
