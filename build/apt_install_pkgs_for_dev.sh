e5ee873e0de82e876fe092fe4447489beb279e60
Hunt Zhan <huntzhan.dev@gmail.com>
2023-11-28 23:32:17 +0800

    chrono: update nodejs build script.

--- build/apt_install_pkgs_for_dev.sh
+++ build/apt_install_pkgs_for_dev.sh
@@ -65,6 +65,7 @@ rm -f /tmp/watchexec-1.22.2-x86_64-unknown-linux-musl.deb
 
 # Install pyright.
 # https://github.com/nodesource/distributions
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
